9e70b6d1bc06de2627a677162bc137768bc9b814
eduardlg <elgcont@gmail.com>
2021-12-16 13:12:49 -0300

    First commit

---
+++ src/br/com/dio/FirstProgramm.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class FirstProgramm {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		Livros livros = new Livros();
+		
+		
+		System.out.println(livros);
+		System.out.println(gato);
+//		int a = 2;
+//		int b = 3;
+//		System.out.println("Hello World! " + (a + b));
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npg;
+}
